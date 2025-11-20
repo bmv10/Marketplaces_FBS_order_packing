cc04ebda8fa4d4b47b87d177870e043ef4308a20
bmv10_Thinkpad_T495s <mbusyukov@gmail.com>
2025-11-20 10:06:35 +0400

    zero working version

--- pack_shipment_ozon.py
+++ pack_shipment_ozon.py
@@ -14,7 +14,7 @@ tokens = eval(os.getenv("OZON_TOKENS"))
 def get_orders_to_shipment(clientID, token, status):
     url = "https://api-seller.ozon.ru/v3/posting/fbs/unfulfilled/list"
     date_ship_to = datetime.datetime.now().strftime("%Y-%m-%d")
-    date_ship_from = (datetime.datetime.now() - datetime.timedelta(days=7)).strftime("%Y-%m-%d")
+    date_ship_from = (datetime.datetime.now() - datetime.timedelta(days=30)).strftime("%Y-%m-%d")
     headers = {"Client-Id": clientID, "Api-Key": token, "Content-Type": "application/json"}
     json = {"dir": "ASC",
             "filter": {
@@ -35,25 +35,25 @@ def get_orders_to_shipment(clientID, token, status):
             }
             }
     r = requests.post(url, headers=headers, json=json)
-    print(r)
+    print("get_orders_to_shipment: ", r)
     load = r.json()
     # print(load)
-    for posting in load.get("result").get("postings"):
-        print(posting)
     posting_numbers = []
     delivery_method = []
     assemble_list = []
     for i in load["result"]["postings"]:
+        print(i)
+        print(i.get("requirements"))
         if i:
             if status == "awaiting_packaging":
-                set_awaiting_deliver_for_order(i, clientID, token)
                 time.sleep(3)
+                set_awaiting_deliver_for_order(i, clientID, token)
             else:
                 assemble_list.append([i.get("posting_number"), i.get("status"), i.get("products")[0].get("offer_id"),
                                       i.get("products")[0].get("quantity")])
                 posting_numbers.append(i.get("posting_number"))
                 delivery_method.append(i.get("delivery_method").get("id"))
-    print(posting_numbers, delivery_method)
+    print("Posting number/ delivery method : ", posting_numbers, delivery_method)
     return posting_numbers, delivery_method, assemble_list
 
 
@@ -68,13 +68,14 @@ def set_awaiting_deliver_for_order(order, clientID, token):
         for j in range(i.get("quantity")):
             json["packages"].append({"products":
                 [
-                    {"exemplar_info":
-                        [
-                            {"gtd": "string",
-                             "is_gtd_absent": True,
-                             "mandatory_mark": "string"
-                             }
-                        ],
+                    {
+                        # "exemplar_info":
+                        # [
+                        #     {"gtd": "string",
+                        #      "is_gtd_absent": True,
+                        #      "mandatory_mark": "string"
+                        #      }
+                        # ],
                         "product_id": i.get("sku"),
                         "quantity": 1
                     }
@@ -84,7 +85,7 @@ def set_awaiting_deliver_for_order(order, clientID, token):
     url = "https://api-seller.ozon.ru/v4/posting/fbs/ship"
     headers = {"Client-Id": clientID, "Api-Key": token, "Content-Type": "application/json"}
     r = requests.post(url, headers=headers, json=json)
-    print(r)
+    print("set_awaiting_deliver_for_order 1: ", r)
     load = r.json()
     print(load)
 
@@ -94,7 +95,7 @@ def create_package_label(posting_numbers, clientID, token):
     headers = {"Client-Id": clientID, "Api-Key": token, "Content-Type": "application/json"}
     json = {"posting_number": posting_numbers}
     r = requests.post(url, headers=headers, json=json)
-    print(r)
+    print("create_package_label: ", r)
     load = r.json()
     print(load)
     return load.get("result").get("task_id")
@@ -105,7 +106,7 @@ def get_package_label(taskID, shop, clientID, token):
     headers = {"Client-Id": clientID, "Api-Key": token, "Content-Type": "application/json"}
     json = {"task_id": taskID}
     r = requests.post(url, headers=headers, json=json)
-    print(r)
+    print("get_package_label: ", r)
     load = r.json()
     if r.status_code == 200:
         while load.get("result").get("file_url") == "":
@@ -126,8 +127,9 @@ def create_act(delivery_method, clientID, token):
         "departure_date": datetime.datetime.now().strftime("%Y-%m-%dT13:00:00.000Z")
     }
     r = requests.post(url, headers=headers, json=json)
-    print(r)
+    print("create_act: ", r)
     load = r.json()
+    # time.sleep(300)
     return load.get("result").get("id")
 
 def check_act(taskID_act, clientID, token):
@@ -135,9 +137,11 @@ def check_act(taskID_act, clientID, token):
     headers = {"Client-Id": clientID, "Api-Key": token, "Content-Type": "application/json"}
     json = {"id": taskID_act}
     r = requests.post(url, headers=headers, json=json)
-    while r.json().get("status") not in ["FORMED", "CONFIRMED", "CONFIRMED_WITH_MISMATCH"]:
-        time.sleep(20)
-        check_act(taskID_act, clientID, token)
+    # while r.json().get("status") not in ["FORMED", "CONFIRMED", "CONFIRMED_WITH_MISMATCH"]:
+    print("check_act: ", r)
+    print(r.text)
+    # time.sleep(20)
+    # check_act(taskID_act, clientID, token)
     return r.json().get("status")
 
 
@@ -147,30 +151,57 @@ def get_package_act(taskID_act, shop, clientID, token):
     json = {"id": taskID_act,
             "doc_type": "act_of_acceptance"}
     r = requests.post(url, headers=headers, json=json)
+    print("get_package_act: ",r)
+    print(r.text)
     print(r)
+    print(r.content)
     load = r.content
     with open(act_name := f'{datetime.datetime.now().strftime("%Y.%m.%d")} OZON {shop} Act.pdf', 'wb') as f:
         f.write(load)
     return act_name
+
+def get_pakage_barcode(taskID, shop, clientID, token):
+    url = "https://api-seller.ozon.ru/v2/posting/fbs/act/get-barcode"
+    headers = {"Client-Id": clientID, "Api-Key": token, "Content-Type": "application/json"}
+    json = {"id": taskID}
+    r = requests.post(url, headers=headers, json=json)
+    print("get_package_barcode: ", r)
+    print(r.text)
+    print(r)
+    print(r.content)
+    load = r.content
+    with open(act_name := f'{datetime.datetime.now().strftime("%Y.%m.%d")} OZON {shop} barcode.png', 'wb') as f:
+        f.write(load)
+    return act_name
+
+#203366890
+# delivery_id = "1020001819824000"
+# delivery_id = "203366890"
+# shop, clientID, token = tokens[0]
+#
+# print(get_pakage_barcode(delivery_id, shop, clientID, token))
+
 
 def make_assemble_list(shop, assembly_sheet):
     if assembly_sheet:
         assembly_sheet = [["Posting number", "Order status", "SKU", "Quantity"]] + assembly_sheet
 
         class PDF(FPDF):
-            def header(self):
-                # Arial bold 15
-                self.set_font('Arial', 'B', 15)
-                # Title
-                self.cell(60, 5, f'OZON - {shop}')
-                # self.cell(30, 10, str(shop))
-                self.set_font('Arial', 'B', 12)
-                self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'R')
-                self.ln()
-                # Date
-                self.cell(30, 10, f"{datetime.datetime.now().strftime('%d-%m-%Y')}")
-                # Line break
-                self.ln(10)
+
+            colontitle = None
+            # def header(self):
+            #     # Arial bold 15
+            #     self.set_font('Arial', 'B', 15)
+            #     # Title
+            #     self.cell(60, 5, f'OZON - {shop}')
+            #     # self.cell(30, 10, str(shop))
+            #     self.set_font('Arial', 'B', 12)
+            #     self.cell(0, 10, 'Page ' + str(self.page_no()), 0, 0, 'R')
+            #     self.ln()
+            #     # Date
+            #     self.cell(30, 10, f"{datetime.datetime.now().strftime('%d-%m-%Y')}")
+            #     # Line break
+            #     self.ln(10)
 
         pdf = PDF('P', 'mm', 'A4')
         pdf.alias_nb_pages()
@@ -182,7 +213,7 @@ def make_assemble_list(shop, assembly_sheet):
             pdf.cell(70, 10, str(sku), 1)
             pdf.cell(20, 10, str(quantity), 1)
             pdf.ln()
-        pdf.output(assembly_list_name:=f"{datetime.datetime.now().strftime('%Y.%m.%d')} OZON {shop} Assemble list.pdf")
+        pdf.output(assembly_list_name := f"{datetime.datetime.now().strftime('%Y.%m.%d')} OZON {shop} Assemble list.pdf")
         return assembly_list_name
 
 
@@ -195,11 +226,11 @@ def main():
             task_id_labels = create_package_label(posting_numbers, clientID, token)
             for delivery_method_id in set(delivery_method):
                 task_id_act = create_act(delivery_method_id, clientID, token)
-                check_act(task_id_act, clientID, token)
+                # check_act(task_id_act, clientID, token)
                 file_list_for_distributer.append(get_package_act(task_id_act, shop, clientID, token))
             file_list_for_distributer.append(get_package_label(task_id_labels, shop, clientID, token))
             print(shop, assemble_list)
-            file_list_for_distributer.append(make_assemble_list_xls("OZON" ,shop, assemble_list))
+            file_list_for_distributer.append(make_assemble_list_xls("OZON", shop, assemble_list))
     send_mail(file_list_for_distributer)
     send_yadisk(file_list_for_distributer)
 
